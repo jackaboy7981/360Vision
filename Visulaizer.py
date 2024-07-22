a0ad4bd8860df02f1b6f55ddae941380302feb53
JACKABOY\jacob <jacobabraham7981@gmail.com>
2024-07-22 19:23:56 +0530

    List of objects, Validations and cross on front side of object

--- Visulaizer.py
+++ Visulaizer.py
@@ -13,9 +13,11 @@ def _load_config():
             _config = json.load(file)
     return _config
 
-def visualizeFrame(xCord, yCord, lenght, width, height, angle, category):
-    '''Visualizing the 3d object position of surrounding objects along with the '''
-    if category == 'Main':
+def visualizeFrame(objects):
+    '''Visualizing the 3d object position of surrounding objects along with the Main object [xCord, yCord, lenght, width, height, angle, category]'''
+    if objects.shape[1] != 7:
+        raise ValueError("Expected objects parameter dimension is [x,7]")
+    if "Main" in objects[:,6]:
         raise ValueError("Category cannot be Main")
     config = _load_config()
     fig = plt.figure()
@@ -28,7 +30,8 @@ def visualizeFrame(xCord, yCord, lenght, width, height, angle, category):
     ax.set_zlabel("Z")
 
     drawBox(ax, 0,0, 4, 4, 2, 0, 'Main') #sensor car box
-    drawBox(ax, xCord, yCord, lenght, width, height, angle, category)
+    for xCord, yCord, lenght, width, height, angle, category in objects:
+        drawBox(ax, xCord, yCord, lenght, width, height, angle, category)
     plt.show()
 
 def drawBox(ax, xCord, yCord, lenght, width, height, angle, category, debugging = False):
@@ -44,9 +47,16 @@ def drawBox(ax, xCord, yCord, lenght, width, height, angle, category, debugging 
     translatedBaseCordinates = np.concatenate((np.array([roatatedCordinates[0] + xCord, roatatedCordinates[1] + yCord, np.zeros(roatatedCordinates[1].shape[0])]), np.array([roatatedCordinates[0] + xCord, roatatedCordinates[1] + yCord, np.full(roatatedCordinates[1].shape[0],height)])), axis=1)
     #translatedBaseCordinates has the base 4 cordinates and then the top 4 cordinates in the same order
     #setting the ploting path
-    plotingPath = np.concatenate((translatedBaseCordinates[:,:4],translatedBaseCordinates[:,:1],translatedBaseCordinates[:,4:],translatedBaseCordinates[:,4:6],translatedBaseCordinates[:,1:3],translatedBaseCordinates[:,6:8],translatedBaseCordinates[:,3:4],translatedBaseCordinates[:,:1],translatedBaseCordinates[:,5:6]), axis=1)
+    plotingPath = np.concatenate((translatedBaseCordinates[:,:4],translatedBaseCordinates[:,:1],translatedBaseCordinates[:,4:],translatedBaseCordinates[:,4:6],translatedBaseCordinates[:,1:3],translatedBaseCordinates[:,6:8],translatedBaseCordinates[:,3:4]), axis=1)
     
     ax.plot(plotingPath[0], plotingPath[1], plotingPath[2], color = config['CategoryColors'][category])
+    #ploting x for front side
+    crossline1 = np.concatenate((translatedBaseCordinates[:,:1],translatedBaseCordinates[:,5:6]),axis=1)
+    crossline2 = np.concatenate((translatedBaseCordinates[:,1:2],translatedBaseCordinates[:,4:5]),axis=1)
+
+    ax.plot(crossline1[0], crossline1[1], crossline1[2], color = "Black")
+    ax.plot(crossline2[0], crossline2[1], crossline2[2], color = "Black")
+
     if debugging:
         colors = ['red', 'blue', 'green', 'yellow']
         fig = plt.figure()
@@ -72,5 +82,5 @@ def drawBox(ax, xCord, yCord, lenght, width, height, angle, category, debugging 
         print("Rotated Base Cordinates", roatatedCordinates)
         print("Translated Base Cordinates", translatedBaseCordinates)
 
-#visualizeFrame(5,7, 4, 4, 2, -np.pi/4, 'Car')
+#visualizeFrame(np.array([[5,7, 4, 4, 2, -np.pi/4, 'Car'],[-5,-7, 4, 7, 2, np.pi/4, 'Car']], dtype=object))
 #drawBox( -4, -4, 4, 4 , 2, np.pi/4, False)
